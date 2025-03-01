a06b6966914dae329a78f5d764bafe251b9cbf7a
Wang Ziyu <1172789766@qq.com>
2025-03-01 17:09:45 +0800

    [perf]修改采集部分代码,优化体验

--- eval.sh
+++ eval.sh
@@ -1,15 +1,17 @@
 HF_USER=$(huggingface-cli whoami | head -n 1)
 
+echo "HF_USER: ${HF_USER}"
+
 python lerobot/scripts/control_robot.py record \
   --robot-path lerobot/configs/robot/so100.yaml \
   --fps 30 \
-  --repo-id ${HF_USER}/eval_act_so100_pickplace_0227 \
+  --repo-id ${HF_USER}/eval_act_so100_fold_0227 \
   --tags so100 tutorial eval \
   --warmup-time-s 5 \
   --episode-time-s 60 \
   --reset-time-s 3 \
   --num-episodes 20 \
-  -p outputs/train/so100_pickplace_0223/checkpoints/last/pretrained_model \
+  -p outputs/train/so100_fold_0227/checkpoints/080000/pretrained_model \
   --single-task "Press the button."  \
   --policy-overrides device=mps
   
